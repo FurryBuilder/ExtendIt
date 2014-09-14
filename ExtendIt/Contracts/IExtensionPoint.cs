be0ebcf03e1c544486e4e3c92f0b97486ca16dce
Etienne Maheu <etienne.maheu@furrybuilder.com>
2014-09-14 01:40:31 -0400

    Initial commit

---
+++ ExtendIt/Contracts/IExtensionPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExtendIt.Contracts
+{
+	/// <summary>
+	/// Encapsulate a set of extension methods in a specific scope (namespace)
+	/// represented by the implementer of this interface.
+	/// </summary>
+	public interface IExtensionPoint
+	{
+		object ExtendedValue { get; }
+
+		Type ExtendedType { get; }
+	}
+}
---
+++ ExtendIt/ExtensionPointBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ExtendIt.Contracts;
+using JetBrains;
+
+namespace ExtendIt
+{
+	/// <summary>
+	/// Base class uppon which specific extension points inherits.
+	/// 
+	/// Encapsulate the scope of an extension point.
+	/// </summary>
+	/// <typeparam name="T">Extension's scope. This can also be object or forwarded using generics.</typeparam>
+	public class ExtensionPointBase<T> : IExtensionPoint
+	{
+		private readonly T _value;
+		private readonly Type _type;
+
+		protected ExtensionPointBase([CanBeNull] T value)
+		{
+			_value = value;
+		}
+
+		protected ExtensionPointBase([NotNull] Type type)
+		{
+			if (!typeof(T).IsAssignableFrom(type))
+			{
+				throw new ArgumentException("type");
+			}
+
+			_type = type;
+		}
+
+		public T ExtendedValue
+		{
+			get { return _value; }
+		}
+
+		object IExtensionPoint.ExtendedValue
+		{
+			get { return _value; }
+		}
+
+		public Type ExtendedType
+		{
+			get { return _type ?? (_value == null ? typeof(T) : _value.GetType()); }
+		}
+	}
+}
