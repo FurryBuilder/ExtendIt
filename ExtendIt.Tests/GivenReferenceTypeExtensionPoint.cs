61c48ef8d22e2d643cef19e0eed6c360d36f31fe
Etienne Maheu <etienne.maheu@furrybuilder.com>
2014-09-14 14:07:22 -0400

    Added a set of unit tests

---
+++ ExtendIt.Tests/GivenReferenceTypeExtensionPoint.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using ExtendIt.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtendIt.Tests
+{
+	[TestClass]
+	public class GivenReferenceTypeExtensionPoint
+	{
+		private class StubExtendable { }
+
+		private class StubInheritingExtendable : StubExtendable { }
+
+		private class MockExtensionPoint : ExtensionPointBase<StubExtendable>
+		{
+			public MockExtensionPoint(StubExtendable value) : base(value) { }
+			public MockExtensionPoint(Type type) : base(type) { }
+		}
+
+		[TestMethod]
+		public void WhenExtendByValue_ThenValueIsSet()
+		{
+			var v = new StubExtendable();
+
+			Assert.AreSame(v, new MockExtensionPoint(v).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendByValue_ThenGenericValueIsSameAsTyped()
+		{
+			var v = new StubExtendable();
+
+			Assert.AreSame(new MockExtensionPoint(v).ExtendedValue, (new MockExtensionPoint(v) as IExtensionPoint).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendedByValue_ThenTypeIsSet()
+		{
+			var v = new StubExtendable();
+
+			Assert.AreEqual(typeof(StubExtendable), new MockExtensionPoint(v).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendedByNullValue_ThenTypeIsSet()
+		{
+			StubExtendable v = null;
+
+			Assert.AreEqual(typeof(StubExtendable), new MockExtensionPoint(v).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendedByInheritedValue_ThenTypeIsKept()
+		{
+			var v = new StubInheritingExtendable();
+
+			Assert.AreEqual(v.GetType(), new MockExtensionPoint(v).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendByType_ThenValueIsDefault()
+		{
+			var t = typeof(StubExtendable);
+
+			Assert.AreEqual(default(StubExtendable), new MockExtensionPoint(t).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendByType_ThenTypeIsSet()
+		{
+			var t = typeof(StubExtendable);
+
+			Assert.AreEqual(t, new MockExtensionPoint(t).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendByInheritedType_ThenTypeIsKept()
+		{
+			var t = typeof(StubInheritingExtendable);
+
+			Assert.AreEqual(t, new MockExtensionPoint(t).ExtendedType);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void WhenExtendedByWrongType_ThenThrow()
+		{
+			new MockExtensionPoint(typeof(object));
+		}
+	}
+}
---
+++ ExtendIt.Tests/GivenValueTypeExtensionPoint.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ExtendIt.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtendIt.Tests
+{
+	[TestClass]
+	public class GivenValueTypeExtensionPoint
+	{
+		private class MockExtensionPoint : ExtensionPointBase<int>
+		{
+			public MockExtensionPoint(int value) : base(value) { }
+			public MockExtensionPoint(Type type) : base(type) { }
+		}
+
+		[TestMethod]
+		public void WhenExtendByValue_ThenValueIsSet()
+		{
+			var i = 3;
+
+			Assert.AreEqual(i, new MockExtensionPoint(i).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendByValue_ThenGenericValueIsEqualAsTyped()
+		{
+			var i = 3;
+
+			Assert.AreEqual(new MockExtensionPoint(i).ExtendedValue, (new MockExtensionPoint(i) as IExtensionPoint).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendedByValue_ThenTypeIsSet()
+		{
+			var i = 3;
+
+			Assert.AreEqual(i.GetType(), new MockExtensionPoint(i).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendedByDefaultValue_ThenTypeIsSet()
+		{
+			var i = 0;
+
+			Assert.AreEqual(typeof(int), new MockExtensionPoint(i).ExtendedType);
+		}
+
+		[TestMethod]
+		public void WhenExtendByType_ThenValueIsDefault()
+		{
+			var t = typeof(int);
+
+			Assert.AreEqual(default(int), new MockExtensionPoint(t).ExtendedValue);
+		}
+
+		[TestMethod]
+		public void WhenExtendByType_ThenTypeIsSet()
+		{
+			var t = typeof(int);
+
+			Assert.AreEqual(t, new MockExtensionPoint(t).ExtendedType);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void WhenExtendedByWrongType_ThenThrow()
+		{
+			new MockExtensionPoint(typeof(double));
+		}
+	}
+}
